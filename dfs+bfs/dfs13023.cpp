8e60c2f9ee6892fa2958fe7248b51dfea207ad29
mjk25 <winsomjk@gmail.com>
2024-02-10 21:31:02 +0900

    sol: dfs 13023

--- dfs+bfs/dfs13023.cpp
+++ dfs+bfs/dfs13023.cpp
@@ -8,6 +8,7 @@ using namespace std;
 static vector <vector<int>> A;
 static vector <bool> visited;
 static bool arrive;
+void DFS(int now, int depth);
 
 int main(){
     freopen("input.txt", "r", stdin);
@@ -17,16 +18,43 @@ int main(){
     cin >> n >> m;
 
     arrive = false;
-    A.resize(n);
+    A.resize(n);    
     visited = vector<bool>(n, false);
 
-    for (int i=0; i<n; i++){
+    for (int i=0; i<m; i++){    //벡터에 노드관계담기
         int s,e;
         cin >> s >> e;
-        //양방향 저장
+        //양방향 저장 ***
         A[s].push_back(e);
         A[e].push_back(s);
     }
 
+    for (int k=0; k<n; k++){
+        DFS(k,1);
+        if (arrive) break;
+    }
 
+    // 종료
+    if (arrive){
+        cout << 1 << "\n";
+    }
+    else {
+        cout << 0 << "\n";
+    }
+}
+
+void DFS(int now, int depth){
+    if (depth==5||arrive){  //종료 조건
+        arrive = true;
+        return;
+    }
+    visited[now] = true;
+
+    for (int node: A[now]){ //재귀
+        if (!visited[node])
+            DFS(node, depth+1);
+        }
+    
+    // 재귀 중 종료조건에 도달하지못한 경우: 새 시작을 위해
+    visited[now] = false;
 }
