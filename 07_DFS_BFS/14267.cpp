ded4cc41b2a7d3481a9bd1ff2eeafa265fcc9e2f
mjk25 <winsomjk@gmail.com>
2024-08-20 23:55:38 +0900

    Sol: 14267

---
+++ 07_DFS_BFS/14267.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+void DFS();
+
+int n,m;
+int senior;
+int i, w;
+vector <int> worker;
+vector <int> compli;
+
+int main(){
+    freopen("/home/user/cpp_baekjoon/input.txt", "r", stdin);
+    cin >>n>>m;
+    worker.resize(n+1);
+    compli.resize(n+1, 0);
+
+    for (int j=1; j<n+1; j++){
+        cin >> senior;
+        if (senior ==-1){
+            worker[0] =j;
+        }else{
+            worker[senior] = j;
+        }  
+    }
+
+    for (int j=0; j<m; j++){
+        cin >> i >> w;
+        compli[i] +=w;
+    }
+    DFS();
+
+    for (int j=1; j<n+1; j++){
+        cout << compli[j] << " ";
+    }
+    return 0;
+}
+
+void DFS(){
+    for (int j=1; j<n+1; j++){
+        int junior = worker[j];
+        compli[junior] += compli[j];
+    }
+}
