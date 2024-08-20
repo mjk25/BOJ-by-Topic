b74961ccedf352fd6b001b4b56c910893ce6f093
mjk25 <winsomjk@gmail.com>
2024-08-21 00:46:08 +0900

    Sol: re 14267

--- 07_DFS_BFS/14267.cpp
+++ 07_DFS_BFS/14267.cpp
@@ -8,27 +8,27 @@ void DFS();
 int n,m;
 int senior;
 int i, w;
-vector <int> worker;
+vector <vector<int>> worker;
 vector <int> compli;
 
 int main(){
     freopen("/home/user/cpp_baekjoon/input.txt", "r", stdin);
     cin >>n>>m;
-    worker.resize(n+1);
+    worker.resize(n+1, vector<int> (0,0));
     compli.resize(n+1, 0);
 
     for (int j=1; j<n+1; j++){
         cin >> senior;
         if (senior ==-1){
-            worker[0] =j;
+            worker[0].push_back(j);
         }else{
-            worker[senior] = j;
+            worker[senior].push_back(j);
         }  
     }
 
     for (int j=0; j<m; j++){
         cin >> i >> w;
-        compli[i] +=w;
+        compli[i] += w;
     }
     DFS();
 
@@ -39,8 +39,9 @@ int main(){
 }
 
 void DFS(){
-    for (int j=1; j<n+1; j++){
-        int junior = worker[j];
-        compli[junior] += compli[j];
+    for (int j=1; j<n; j++){
+        for (int junior : worker[j]){
+            compli[junior] += compli[j];
+        }
     }
 }
